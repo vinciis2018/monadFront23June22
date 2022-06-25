937fa9f9e1ed93be131327c3503b70722d730f9e
vinciis2018 <vinciis2018@gmail.com>
2022-06-26 03:56:11 +0530

    18 commit

--- client/src/pages/screens/screenPlayer.tsx
+++ client/src/pages/screens/screenPlayer.tsx
@@ -61,9 +61,9 @@ export function ScreenPlayer (props: any) {
         <MessageBox variant="danger">{errorScreenVideos}</MessageBox>
       ) : (
         <video 
-          autoPlay={true}
-          
-          src="https://arweave.net/DGcP1bUjPZ5BKRegD5PFb94C_wO4HPZ2mq236p6Il70"
+          autoPlay
+          controls
+          src={"https://arweave.net/DGcP1bUjPZ5BKRegD5PFb94C_wO4HPZ2mq236p6Il70"}
           onEnded={(e) => looping(e)}
           poster="https://arweave.net/pziELbF_OhcQUgJbn_d1j_o_3ASHHHXA3_GoTdJSnlg"
           width="100%"
